53de8597daad81c0c1c97fa6f1cc05f7230d6c93
Belen Yarde Buller <belenyardebuller@gmail.com>
2023-04-26 11:04:28 -0300

    Bootstrap js working and list products by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
+  def initialize
+    super
+    @backend_service = BackendService.new
+  end
+
   def index
+    items = @backend_service.get_items()
+    @items_by_category = items.group_by { |item| item["category"] }
   end
 end
