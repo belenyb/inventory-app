f2531d5a30187dcfdd86bcd2d4399cb1c9419d4b
Belen Yarde Buller <belenyardebuller@gmail.com>
2023-05-03 14:44:51 -0300

    Show product categories in cards on index products page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,21 @@
 module ProductsHelper
+  def get_title_emoji(title)
+    title = title.downcase
+    emoji = ""
+    case title
+      when "alimentos"
+        emoji = "🍖"
+      when "autos"
+        emoji = "🚗"
+      when "juegos"
+        emoji = "🧩"
+      when "ropa"
+        emoji = "👕"
+      when "tech"
+        emoji = "📱"
+      else
+        emoji = "📦"
+    end
+    return emoji
+  end
 end
