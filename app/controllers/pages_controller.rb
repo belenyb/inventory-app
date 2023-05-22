aa80c22881286d144a594801b7d18d7288ea1e94
Belen Yarde Buller <belenyardebuller@gmail.com>
2023-05-22 17:52:05 -0300

    Devise installed, singup and singin custom pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+    if user_signed_in?
+      redirect_to dashboard_index_path
+    else
+      redirect_to new_user_session_path
+    end
+
+  end
+end
