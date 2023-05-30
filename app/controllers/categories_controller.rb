07615b7f1f9834dfc6b35ebe31ab3bc045b89c6d
Belen Yarde Buller <belenyardebuller@gmail.com>
2023-05-30 14:47:35 -0300

    Category detail page created and directed from categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,5 +10,9 @@ class CategoriesController < ApplicationController
     end
 
     def show
+      all_items = @backend_service.get_items()
+      items_by_category = all_items.group_by { |item| item["category"] }
+      @category = params[:id]
+      @items = items_by_category[@category] || []
     end
 end
