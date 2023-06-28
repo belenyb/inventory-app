c1d93ac8c394b25a3d9f145689205f6dfde64680
Belen Yarde Buller <belenyardebuller@gmail.com>
2023-06-28 17:01:35 -0300

    DB migrated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_22_194138) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.datetime "_created"
     t.string "_data_type"
